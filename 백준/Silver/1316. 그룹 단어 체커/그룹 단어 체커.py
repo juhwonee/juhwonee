89cb3610c1eadc234f9316641594bb26342091b9
JuHwon Jeong <7585sy@naver.com>
2026-01-13 13:42:28 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+for i in range(N):
+    word = input()
+    for j in range(len(word)-1):
+        if word[j] == word[j+1]:
+            continue
+        elif word[j] in word[j+1:]:
+            N -= 1
+            break
+        else:
+            continue
+
+print(N)
