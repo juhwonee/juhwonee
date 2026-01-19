21ea3632fc816285503f9b88002c5030b8a261a3
JuHwon Jeong <7585sy@naver.com>
2026-01-20 08:45:52 +0900

    [Silver IV] Title: 수 찾기, Time: 152 ms, Memory: 52424 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+N_list = set(map(int, input().split()))  # set으로 변경
+
+M = int(input())
+M_list = list(map(int, input().split()))
+
+for i in M_list:
+    if i in N_list:
+        print(1)
+    else:
+        print(0)
