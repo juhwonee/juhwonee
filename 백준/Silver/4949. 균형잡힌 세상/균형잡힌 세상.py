7426b8ea341c7ea3305e0ce2351dbfa2bc268f2e
JuHwon Jeong <7585sy@naver.com>
2026-01-20 14:10:42 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 248 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,27 @@
+while 1:
+    S = input()
+    stack = []
+
+    if S == '.':
+        break
+
+    for i in S:
+        if i == '[' or i == '(':
+            stack.append(i)
+
+        elif i == ']':
+            if len(stack)!=0 and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(i)
+
+        elif i == ')':
+            if len(stack)!=0 and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(i)
+
+    if len(stack) == 0:
+        print('yes')
+    else:
+        print('no')
