2bd3a86aeb47b5e889435f91931e796a35d5b8b6
JuHwon Jeong <7585sy@naver.com>
2026-01-23 13:11:20 +0900

    [Silver III] Title: 소수 구하기, Time: 5876 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -7,8 +7,10 @@ for i in range(N, M+1):
     if i == 1:
         continue
 
+    breaking = True
     for j in range(2, int(i**0.5) +1):
         if i % j == 0:
+            breaking = False
             break
-    else:
+    if breaking == True:
         print(i)
