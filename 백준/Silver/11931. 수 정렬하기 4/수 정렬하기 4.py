14c816a5db5e68d7014d5798b1524967423f73bc
JuHwon Jeong <7585sy@naver.com>
2026-02-24 21:45:09 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1372 ms, Memory: 78268 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+num = []
+
+for _ in range(N):
+    num.append(int(input()))
+
+num.sort()
+for i in range(N-1,-1,-1):
+    print(num[i])
