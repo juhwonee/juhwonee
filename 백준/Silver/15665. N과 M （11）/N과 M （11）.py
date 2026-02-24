f1db440c31c02f5207a51ea6acdc5f04c8ffdfcc
JuHwon Jeong <7585sy@naver.com>
2026-02-24 11:11:10 +0900

    [Silver II] Title: N과 M (11), Time: 928 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -0,0 +1,22 @@
+N, M = map(int, input().split())
+arr = list(map(int, input().split())) # 9 7 9 1
+arr.sort() # 1 7 9 9
+result = []
+
+def solution():
+    if len(result) == M:
+        for i in result:
+            print(arr[i], end=' ')
+        print()
+        return()
+
+    prev = 0
+    for i in range(N):
+        if arr[i] != prev:
+            result.append(i)
+            prev = arr[i]
+
+            solution()
+            result.pop()
+
+solution()
